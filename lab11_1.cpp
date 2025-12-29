76d2e7ed06e0bf7af14fd3ff9ee32a5a4d7d130a
iamjanenough <jannilover20@gmail.com>
2025-12-30 01:19:29 +0700

    LAB11.1

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,30 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    string temp;
+
+    cout << "Press Enter 3 times to reveal your future." << endl << endl;
+
+    
+    for (int i = 0; i < 3; i++) {
+        getline(cin, temp);
+    }
+
+    
+    string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    int size = 9;
+
+    
+    srand(time(0));
+    int r = rand() % size;
+
+    cout << "You will get " << grade[r] << " in this 261102." << endl;
+
+    return 0;
+}
+
